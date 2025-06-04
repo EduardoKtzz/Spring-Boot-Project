586b3a8fa9650d0f99104073aa5f32b7dda91188
Eduardo Klitzke Oliveira <EduardoKlitzkee@gmail.com>
2025-06-04 20:15:37 -0300

    Adicionando configurações de dependências, JPA repository e inserindo dados no h2 (Aula4)

---
+++ src/main/java/com/klitzke/aula/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.klitzke.aula.repositories;
+
+import com.klitzke.aula.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+
+
+
+}

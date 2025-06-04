5f6194aad87635987655b68b9c8fffc9493d04df
Eduardo Klitzke Oliveira <EduardoKlitzkee@gmail.com>
2025-06-04 20:35:17 -0300

    Adicionando dependências, novos métodos para buscar usuarios, e um novo caminho GET (Aula5)

--- src/main/java/com/klitzke/aula/repositories/UserRepository.java
+++ src/main/java/com/klitzke/aula/repositories/UserRepository.java
@@ -2,11 +2,9 @@ package com.klitzke.aula.repositories;
 
 import com.klitzke.aula.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
-
-
-
 
 }
---
+++ src/main/java/com/klitzke/aula/services/UserServices.java
@@ -0,0 +1,28 @@
+package com.klitzke.aula.services;
+
+import com.klitzke.aula.entities.User;
+import com.klitzke.aula.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServices {
+
+    @Autowired
+    private UserRepository repository;
+
+    //Buscar todos os users
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    //Buscar usuário por 'ID'
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+
+}
